f6b73cb04f7b0baebebc271323f16db14c3f2c5c
taymaz <75164763+taymazmostafaei@users.noreply.github.com>
2023-02-14 18:46:49 +0330

    add id shorter

--- controller/OtherWise.js
+++ controller/OtherWise.js
@@ -1,6 +1,11 @@
 const Telegram = require('telegram-node-bot')
 const lyrics = require('../model/Lyrics')
 const ejs = require('ejs');
+const ObjectID = require('mongodb').ObjectID;
+var ShortId = require('id-shorter');
+var mongoDBId = ShortId({
+    isFullId: true
+});
 
 const TelegramBaseController = Telegram.TelegramBaseController
 
@@ -9,7 +14,25 @@ class OtherWise extends TelegramBaseController {
      * @param {Scope} $
      */
     async handle($) {
+        
+        if (($.message.text).length < 3) {
+
+            await $.sendMessage('🤏')
+            await $.sendMessage('axtarmaq üçün azı 3 karakter gələklidir.')
+            return;
+        }
+
         let ly = new lyrics()
+        if (($.message.text).split('_')[0] == '/g') {
+
+            let sId = ($.message.text).split('_')[1]
+            let lyricsId = mongoDBId.decode(sId)
+
+            let lyric = await ly.findOne({ _id: ObjectID(lyricsId) })
+
+            await $.sendMessage(lyric.lyrics)
+            return;
+        }
 
         await $.sendMessage('Axtarmaq başlandı🔎')
 
@@ -22,7 +45,7 @@ class OtherWise extends TelegramBaseController {
             return;
         }
 
-        ejs.renderFile('./template/search.ejs', { lyrics: result }, null, (err, message) => {
+        ejs.renderFile('./template/search.ejs', { lyrics: result, search: mongoDBId }, null, (err, message) => {
             $.sendMessage(message || err)
         });
 
